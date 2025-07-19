6e5d1ff94a0e3a6d073362cc06a2301948ec7657
Damilola640 <Olatoyeclement531@gmail.com>
2025-07-19 14:31:43 +0100

    new changes

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    publication_year = models.DateField()
+    title = models.CharField(max_length=200, default="Untitled")
+    author = models.CharField(max_length=100, default="Unknown Author")
+    publication_year = models.IntegerField(default=2000)
 
     def __str__(self):
         return self.title
