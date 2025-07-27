72f63c88747d67c53399a1c0288208fa17a0d7a5
Damilola640 <Olatoyeclement531@gmail.com>
2025-07-27 22:47:53 +0100

    fix: remove extra space in CustomUser class definition in models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -12,7 +12,7 @@ class Book(models.Model):
         return self.title
 
     # Additional Information (Using AbstractUser).
-    class CustomUser (AbstractUser):
+    class CustomUser(AbstractUser):
         date_of_birth = models.DateField(null=True, blank=True)
         profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
 
