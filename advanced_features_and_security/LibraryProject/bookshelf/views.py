c794cc206a88232b8a73f5e8c0fadeb4a52d6403
Damilola640 <Olatoyeclement531@gmail.com>
2025-07-27 23:03:23 +0100

    add: implement book list view with permission checks and search functionality

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Book
+from django.contrib.auth.decorators import permission_required
+from .forms import ExampleForm
+
 
 # Create your views here.
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    form = ExampleForm(request.GET or None)
+    books = Book.objects.all()
+    if form.is_valid():
+        query = form.cleaned_data.get('title')
+        if query:
+            books = books.filter(title__icontains=query)
+    return render(request, 'bookshelf/book_list.html', {'books': books, 'form': form})
