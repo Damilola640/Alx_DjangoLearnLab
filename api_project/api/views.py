5cae4e12c2482c7c7d08630614e610531bd6a6e3
Damilola640 <Olatoyeclement531@gmail.com>
2025-08-02 19:11:02 +0100

    work in progress

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
-from api_project.api.serializers import BookSerializer
+from .serializers import BookSerializer
 
 # Create your views here.
-class BookListView(generics.ListCreateAPIView):
+class BookListView(generics.ListAPIView, "from .serializers import BookSerializer"):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vn!#k+r@9w&b1)+6l=$i9kpl&oh8o26dxj7n1nig75g!sw+2)-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://localhost:8000/']
 
 
 # Application definition
