ae236c458eac8be18207174c80b2e3dbf488cf5f
Damilola640 <Olatoyeclement531@gmail.com>
2025-08-02 19:31:35 +0100

    viewsets and permissions

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
 # Create your views here.
-class BookListView(generics.ListAPIView, "from .serializers import BookSerializer"):
+class BookListView(generics.ListAPIView, viewsets.GenericViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [IsAdminUser()]
+        return [IsAuthenticated()]
