dfa70dfb699f0362fa811dd58d97f0a4ee7d0ca8
Damilola640 <Olatoyeclement531@gmail.com>
2025-08-17 22:46:07 +0100

    models was created

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+# Author model: Represents a writer who can have multiple books
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+# Book model: Represents a book linked to an Author
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(
+        Author,
+        related_name="book",  # Enables Author.book to access related books
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"{self.title} ({self.publication_year})"
