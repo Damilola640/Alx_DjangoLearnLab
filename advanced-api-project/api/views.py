a61310f9ed0645833d249fc0996bf66c0e1be8f1
Damilola640 <Olatoyeclement531@gmail.com>
2025-08-19 23:08:54 +0100

    imported django filters

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics, permissions
 from rest_framework import filters
 from rest_framework.filters import SearchFilter,OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework as filters
 
 from .models import Book
 from .serializers import BookSerializer
