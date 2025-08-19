86be6d1e43599780916a33048cf30991ba86f2c9
Damilola640 <Olatoyeclement531@gmail.com>
2025-08-19 23:41:08 +0100

    configured database

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',  # Replace with your desired database name
+        'USER': 'damilola640',  # Replace with your PostgreSQL username
+        'PASSWORD': 'Boluwatife1996', # Replace with your PostgreSQL password
+        'HOST': 'localhost',  # Or the IP/hostname of your PostgreSQL server
+        'PORT': '5432',  # Leave empty for default (5432) or specify if needed
     }
 }
 
