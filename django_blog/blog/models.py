42137f3b0ecec209caaecac39be66e81c477551b
Damilola640 <Olatoyeclement531@gmail.com>
2025-08-19 06:48:39 +0100

    Django blog models was configured and trailling error for django taggit was fixed

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,3 +1,71 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.db.models.signals import post_save
+from django.dispatch import receiver # Signal to create/update profile automatically
+from taggit.managers import TaggableManager # Optional: If you want to use tags for posts
+import uuid
+import os
 
-# Create your models here.
+def avatar_upload_path(instance, filename):
+    # return f'profile_pics/user_{instance.user.id}/{filename}' # this will create a directory for each user
+    # Generate a unique filename using UUID
+
+    ext = filename.split('.')[-1]  # get file extension
+    unique_filename = f"{uuid.uuid4()}.{ext}"
+    return os.path.join("profile_pics", f"user_{instance.user.id}", unique_filename)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField(blank=True)
+    avatar = models.ImageField(upload_to=avatar_upload_path, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        'auth.User', on_delete=models.CASCADE, related_name='blogs'
+    )
+    tags = TaggableManager(blank=True)  # Optional: If you want to use tags for posts
+    
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ['-created_at']
+    
+    def __str__(self):
+        return f'Comment by {self.author.username} on {self.post.title}'
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    def __str__(self):
+        return self.name
+
+# Signal to automatically create profile when user is created
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    if hasattr(instance, 'profile'):
+        instance.profile.save()
+    else:
+        Profile.objects.create(user=instance)
