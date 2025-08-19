0be9eb0ed2e9c7d744f876b15b3d46446c5e0c01
Damilola640 <Olatoyeclement531@gmail.com>
2025-08-19 22:44:20 +0100

    authentication was added in views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, permissions
 from rest_framework import filters
 from rest_framework.filters import SearchFilter,OrderingFilter
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Book
