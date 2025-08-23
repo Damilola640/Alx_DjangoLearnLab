2cf878dcf475be40c738f0a54c7df83a3bd3134c
Damilola640 <Olatoyeclement531@gmail.com>
2025-08-23 23:15:47 +0100

    url pattern for blog app authentication system

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,32 @@
+from django.urls import path
+from .views import (
+    BlogLoginView, BlogLogoutView, register, profile,
+    PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, CommentCreateView, CommentUpdateView, CommentDeleteView, PostByTagListView, PostSearchView
+)
+from . import views
+
+urlpatterns = [
+    # Authentication URLs
+    path('login/',  BlogLoginView.as_view(),  name='login'),
+    path('logout/', BlogLogoutView.as_view(), name='logout'),
+    path('register/', register, name='register'),
+    path('profile/', profile, name='profile'),
+    
+    # Blog post URLs
+    path('', PostListView.as_view(), name='post-list'),
+    path('posts/', PostListView.as_view(), name='posts'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+
+    # Comments url
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment_create'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment_update'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),
+
+    # Search posts and tags
+    path("search/", PostSearchView.as_view(), name="search-posts"),
+    path("tags/<slug:tag_slug>/", PostByTagListView.as_view(), name="posts_by_tag"),
+
+]
