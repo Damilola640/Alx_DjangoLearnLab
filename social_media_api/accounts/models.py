95e1e64fa9ba55c9ab413da62d6575ef9d576aca
Damilola640 <Olatoyeclement531@gmail.com>
2025-08-25 06:00:51 +0100

    user model and authentication

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    bio = models.TextField(max_length=500, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    followers = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        related_name='user_following',
+        blank=True
+    )
+
+    # users that THIS user is following
+    following = models.ManyToManyField(
+        "self",
+        symmetrical=False,
+        related_name="user_followers",
+        blank=True
+    )
+
+    def follow(self, user):
+        """Follow another user"""
+        if user != self:
+            self.following.add(user)
+
+    def unfollow(self, user):
+        """Unfollow another user"""
+        if user != self:
+            self.following.remove(user)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.username
+
+    @property
+    def follower_count(self):
+        return self.followers.count()
+
+    @property
+    def following_count(self):
+        return self.following.count()
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posts")
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Post by {self.author.username} at {self.created_at}"
