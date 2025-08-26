9fd3cf9b09878accd8ae2022d657ea288c80479c
Damilola640 <Olatoyeclement531@gmail.com>
2025-08-26 09:02:13 +0100

    Revert "started new app posts"
    This reverts commit fb823086fcba36a75141a719feba429b92983298.

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -41,8 +41,6 @@ INSTALLED_APPS = [
     'accounts',  # App for the social media API
 ]
 
-AUTH_USER_MODEL = 'accounts.CustomUser'
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
